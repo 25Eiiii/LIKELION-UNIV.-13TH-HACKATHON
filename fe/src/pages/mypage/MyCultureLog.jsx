b1a5af4edc1a01449b7d7727935dcfd3aec6d9da
yejinkyo <p2006655@gmail.com>
2025-08-05 10:34:52 +0900

    [feat] 내 문화 기록 연동

--- fe/src/pages/mypage/MyCultureLog.jsx
+++ fe/src/pages/mypage/MyCultureLog.jsx
@@ -1,34 +1,32 @@
 import EventCardS from '../../components/EventCardS';
 import React from 'react';
 import styled from 'styled-components';
+import { useMyEvents } from '../../hooks/useMyEvents';
+import useMyEventStore from '../../store/useMyEventStore';
 
 const MyCultureLog = () => {
-  const logs = [
-    {
-        title: "스테이 성북",
-        date: "2025.08.20 - 2025.08.27",
-        location: "성북예술창작터",
-        thumbnail: "post2.svg",
-    },
-    {
-        title: "한옥 쉼표, 싱잉볼 테라피",
-        date: "2025. 08. 20 - 2025. 08. 27",
-        location: "서울 문화 예술 교육 센터",
-        thumbnail: "post3.svg",
-    },
-    {
-        title: "한옥 쉼표, 싱잉볼 테라피",
-        date: "2025. 08. 20 - 2025. 08. 27",
-        location: "서울 문화 예술 교육 센터",
-        thumbnail: "post3.svg",
-    },
-    ];
+  const { isLoading, isError } = useMyEvents();
+  const events = useMyEventStore(state => state.events);
+
+  if (isLoading) return <Wrapper>불러오는 중</Wrapper>;
+  if (isError) return <Wrapper>오류 발생</Wrapper>
+  
     
   return (
     <Wrapper>
-        {logs.map((log, idx) => (
+        {events.map((event, idx) => (
             <CardGroup key={idx}>
-            <EventCardS {...log}/>
+              <CultureItem>
+                  <CultureThumbnail 
+                    src={event.main_img}
+                    alt={event.title}
+                  />
+                  <CultureInfo>
+                      <CultureTitle>{event.title}</CultureTitle>
+                      <CultureDate>{event.date}</CultureDate>
+                      <CultureLocation>{event.place}</CultureLocation>
+                  </CultureInfo>
+              </CultureItem>
             <ReviewBtn>후기 작성하기</ReviewBtn>
             </CardGroup>
         ))}
@@ -63,4 +61,48 @@ align-self: flex-end;
 position: absolute;
 top: 95px;
 box-sizing: border-box;
+`
+
+export const CultureItem = styled.div`
+display: flex;
+gap: 30px;
+`
+
+export const CultureThumbnail = styled.img`
+width: 93px;
+height: 127px;
+flex-shrink: 0;
+`
+
+export const CultureInfo = styled.div`
+display: flex;
+flex-direction: column;
+gap: 10px;
+`
+
+export const CultureTitle = styled.div`
+color: #353535;
+font-family: Pretendard;
+font-size: 17px;
+font-style: normal;
+font-weight: 600;
+line-height: normal;
+`
+
+export const CultureDate = styled.div`
+color: #404040;
+font-family: Pretendard;
+font-size: 13px;
+font-style: normal;
+font-weight: 400;
+line-height: normal;
+`
+
+export const CultureLocation = styled.div`
+color: #848484;
+font-family: Pretendard;
+font-size: 13px;
+font-style: normal;
+font-weight: 400;
+line-height: normal;
 `
