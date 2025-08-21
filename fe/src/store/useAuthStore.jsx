86492f0f2eaa1e674b46b499f25f1b07d706ff6d
yejinkyo <p2006655@gmail.com>
2025-08-21 15:57:49 +0900

    [fix] 새로고침 해도 닉네임 유지

--- fe/src/store/useAuthStore.jsx
+++ fe/src/store/useAuthStore.jsx
@@ -1,8 +1,15 @@
 import { create } from 'zustand'
 
 const useAuthStore = create((set) => ({
-  nickname: '',
-  setNickname: (nickname) => set({ nickname }),
+  nickname: localStorage.getItem('nickname') || '',
+  setNickname: (nickname) => {
+    localStorage.setItem('nickname', nickname);
+    set({ nickname });
+  },
+  clearNickname: () => {
+    localStorage.removeItem('nickname');
+    set({ nickname: '' });
+  }
 }));
 
 export default useAuthStore;
