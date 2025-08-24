2a80baf6cbb988bcb9b18b52dcb652aafa5a75a6
yejinkyo <p2006655@gmail.com>
2025-08-24 18:19:40 +0900

    [fix] 새로고침 없이 삭제 반영

--- fe/src/hooks/useMyReview.jsx
+++ fe/src/hooks/useMyReview.jsx
@@ -5,23 +5,19 @@ import { api } from "../api/fetcher"
 
 // 데이터 불러오기
 const fetchMyReviews = async () => {
-    const { setReviews } = useMyReviewStore.getState();
     const token = localStorage.getItem("accessToken");
     const res = await axios.get("/api/surveys/my-reviews/", {
         headers: {
             Authorization: `Bearer ${token}`,
         },
     });
-    setReviews(res.data);
     return res.data;
 }
 
 // useMyReviews 훅
 export const useMyReviews = () => {
-    const setReviews = useMyReviewStore((state) => state.setReviews);
-
     return useQuery({
-        queryKey: ["myReviews"],
+        queryKey: ["my-reviews"],
         queryFn: fetchMyReviews,
         onSuccess: (data) => setReviews(data),
     });
